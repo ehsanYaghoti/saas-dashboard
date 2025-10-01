76d0d7f8eed23b366c1f46b2b6afa8041659a386
Ehsan <ehsan.yaghoti@yahoo.com>
2025-10-01 16:10:37 +0330

    update sidebar detail styles

--- src/components/common/sidebar.tsx
+++ src/components/common/sidebar.tsx
@@ -102,7 +102,7 @@ export function AppSidebar() {
               >
                 <Logo collapsed={state === "collapsed"} />
                 <h1
-                  className={`font-bold text-2xl text-primary-4 ${
+                  className={`font-bold text-2xl text-primary-4 dark:text-white ${
                     state === "collapsed" && "hidden"
                   } `}
                 >
@@ -126,7 +126,7 @@ export function AppSidebar() {
                       className=" active:hover:bg-primary-1 active:text-white "
                       asChild
                     >
-                      <a href={item.url}>
+                      <a href={item.url} >
                         <item.icon />
                         <span>{item.title}</span>
                       </a>
@@ -142,7 +142,7 @@ export function AppSidebar() {
               <SidebarMenu>
                 {items_2.map((item) => (
                   <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild>
+                    <SidebarMenuButton asChild className=" active:hover:bg-primary-1 active:text-white " >
                       <a href={item.url}>
                         <item.icon />
                         <span>{item.title}</span>
@@ -157,25 +157,24 @@ export function AppSidebar() {
 
         <SidebarFooter className=" px-4">
           <SidebarMenu>
-            <SidebarMenuItem>
-              {/* <SidebarMenuButton> */}
-              <SidebarMenuButton className="group-data-[collapsible=icon]:hidden focus:!bg-primary-1 hover:bg-primary-1 w-full h-[160px] p-4 rounded-md bg-primary-1
+            {/* <SidebarMenuItem> */}
+              <SidebarMenuItem className="group-data-[collapsible=icon]:hidden w-full h-[160px] p-4 rounded-md bg-primary-1
                     relative flex flex-col items-start justify-end gap-3 shadow-2xs shadow-primary-1
                     after:content-[''] after:absolute after:w-44 after:h-44 after:border-2 after:border-black/30 after:rounded-full after:left-[30%] after:-top-[70%]
                     before:content-[''] before:absolute before:w-44 before:h-44 before:border-2 before:border-black/30 before:rounded-full before:left-[80%] before:-top-[45px]
+                    overflow-hidden
                 ">
                 <Ball />
-                <p className=" text-white text-sm  ">
+                <p className=" text-white text-sm   ">
                   Get detailed analitycs to help you, upgrade pro
                 </p>
-                <a href="#" className=" ">
+                {/* <a href="#" className=" "> */}
                   <button className=" bg-white !cursor-pointer hover:bg-primary-1 hover:text-white border border-transparent hover:border-white  rounded-[10px] text-sm text-primary-1 px-2 py-1 ">
                     Upgrade Now
                   </button>
-                </a>
-              </SidebarMenuButton>
-              {/* </SidebarMenuButton> */}
-            </SidebarMenuItem>
+                {/* </a> */}
+              </SidebarMenuItem>
+            {/* </SidebarMenuItem> */}
           </SidebarMenu>
         </SidebarFooter>
         <SidebarRail className="" />
