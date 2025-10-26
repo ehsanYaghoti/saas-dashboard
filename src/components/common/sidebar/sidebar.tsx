1ed082b88fb02aa72eb97fdf16ca29711b9b5146
Ehsan <ehsan.yaghoti@yahoo.com>
2025-10-26 11:18:42 +0330

    ADD react router

--- src/components/common/sidebar/sidebar.tsx
+++ src/components/common/sidebar/sidebar.tsx
@@ -27,18 +27,18 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 
-
 import { Suspense } from "react";
 import NavProjectsSkeleton from "@/components/loadings/skeletons/sidebarSkeleton";
 import { Logo } from "./logo";
 import { cn } from "@/lib/utils";
 import Ball from "./ball";
+import { NavLink, useLocation } from "react-router";
 
 // Menu items.
 const items_1 = [
   {
     title: "Overview",
-    url: "#",
+    url: "/",
     icon: Grid2X2,
   },
   {
@@ -58,7 +58,7 @@ const items_1 = [
   },
   {
     title: "Products",
-    url: "#",
+    url: "/products",
     icon: Package,
   },
   {
@@ -87,6 +87,8 @@ const items_2 = [
 ];
 export function AppSidebar() {
   const { state } = useSidebar();
+  const {pathname} = useLocation();
+
   return (
     <Suspense fallback={<NavProjectsSkeleton />}>
       <Sidebar collapsible="icon" className="font-inter bg-white ">
@@ -100,7 +102,7 @@ export function AppSidebar() {
                     "!p-0 justify-center group-data-[collapsible=icon]:p-0! group-data-[collapsible=icon]:self-center "
                 )}
               >
-                 <Logo collapsed={state === "collapsed"} />
+                <Logo collapsed={state === "collapsed"} />
                 <h1
                   className={`font-bold text-2xl text-primary-4 dark:text-white ${
                     state === "collapsed" && "hidden"
@@ -119,17 +121,21 @@ export function AppSidebar() {
           <SidebarGroup>
             <SidebarGroupLabel>Main menu</SidebarGroupLabel>
             <SidebarGroupContent>
-              <SidebarMenu>
+              <SidebarMenu >
                 {items_1.map((item) => (
                   <SidebarMenuItem key={item.title}>
                     <SidebarMenuButton
-                      className=" active:hover:bg-primary-1 active:text-white "
+                      className=" active:hover:bg-primary-1 active:hover:text-white "
                       asChild
                     >
-                      <a href={item.url} >
-                        <item.icon />
-                        <span>{item.title}</span>
-                      </a>
+                        <NavLink
+                          to={item.url}
+                          end
+                          className={pathname === item.url ? ' text-white bg-primary-1  ' : '' }
+                        >
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </NavLink>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
                 ))}
@@ -142,7 +148,10 @@ export function AppSidebar() {
               <SidebarMenu>
                 {items_2.map((item) => (
                   <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild className=" active:hover:bg-primary-1 active:text-white " >
+                    <SidebarMenuButton
+                      asChild
+                      className=" active:hover:bg-primary-1 active:text-white "
+                    >
                       <a href={item.url}>
                         <item.icon />
                         <span>{item.title}</span>
@@ -158,22 +167,24 @@ export function AppSidebar() {
         <SidebarFooter className=" px-4">
           <SidebarMenu>
             {/* <SidebarMenuItem> */}
-              <SidebarMenuItem className="group-data-[collapsible=icon]:hidden w-full h-[160px] p-4 rounded-md bg-primary-1
+            <SidebarMenuItem
+              className="group-data-[collapsible=icon]:hidden w-full h-[160px] p-4 rounded-md bg-primary-1
                     relative flex flex-col items-start justify-end gap-3 shadow-2xs shadow-primary-1
                     after:content-[''] after:absolute after:w-44 after:h-44 after:border-2 after:border-black/30 after:rounded-full after:left-[30%] after:-top-[70%]
                     before:content-[''] before:absolute before:w-44 before:h-44 before:border-2 before:border-black/30 before:rounded-full before:left-[80%] before:-top-[45px]
                     overflow-hidden
-                ">
-                <Ball />
-                <p className=" text-white text-sm   ">
-                  Get detailed analitycs to help you, upgrade pro
-                </p>
-                {/* <a href="#" className=" "> */}
-                  <button className=" bg-white !cursor-pointer hover:bg-primary-1 hover:text-white border border-transparent hover:border-white  rounded-[10px] text-sm text-primary-1 px-2 py-1 ">
-                    Upgrade Now
-                  </button>
-                {/* </a> */}
-              </SidebarMenuItem>
+                "
+            >
+              <Ball />
+              <p className=" text-white text-sm   ">
+                Get detailed analitycs to help you, upgrade pro
+              </p>
+              {/* <a href="#" className=" "> */}
+              <button className=" bg-white !cursor-pointer hover:bg-primary-1 hover:text-white border border-transparent hover:border-white  rounded-[10px] text-sm text-primary-1 px-2 py-1 ">
+                Upgrade Now
+              </button>
+              {/* </a> */}
+            </SidebarMenuItem>
             {/* </SidebarMenuItem> */}
           </SidebarMenu>
         </SidebarFooter>
